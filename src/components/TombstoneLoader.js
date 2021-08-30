18c73292a96aa644bbf4c23b0f4014db3151a48c
Ryan Rundle <ryan@ocwebkings.com>
2021-08-29 19:23:11 -0700

    remove copied comments

--- src/components/TombstoneLoader.js
+++ src/components/TombstoneLoader.js
@@ -2,17 +2,6 @@ import React from 'react';
 
 /**
  * @TombstoneLoader
- *
- * Placeholder component that takes up the entire height and width
- * of it's parent and presents an animated tombstone box.
- *
- * ```typescript
- * import {TombstoneLoader} from '@ion/dls/src/components/loading';
- *
- * <TombstoneLoader />
- *
- * ```
- *
  */
 export const TombstoneLoader = () => (
   <div className='tombstone-loader'>
